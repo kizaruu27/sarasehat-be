957c61ba42d14d6d246f5f16b5e0045d4a7e18f3
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2025-01-20 20:53:24 +0700

    reset main branch

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import itemRoutes from "./routes/itemsRoutes";
 
 const app: Application = express();
 dotenv.config();
@@ -10,8 +9,6 @@ const PORT: string | number = process.env.PORT || 8000;
 
 // Update later
 app.use(cors());
-
-app.use("/items", itemRoutes);
 
 // Endpoint you'll always need for
 app.get("/ping", (req: Request, res: Response) => {
--- src/routes/itemsRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { getAllItems } from "../controllers/itemsControllers";
-const router = Router();
-
-router.get("/", getAllItems);
-
-export default router;
